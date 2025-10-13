fb3800fbc933d09b9d1da87f9a2192c7f7adf1ee
Stanislav Karpov <stanislav.karpov@palas.de>
2025-10-14 01:20:08 +0200

    test(ui): declare require in custom test bootstrap

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,8 @@
 // Custom Angular test loader limited to app/ specs only.
 import 'zone.js/testing';
+// Declare require for bundler/ESM TS compile (Angular test builder still provides webpack require)
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare const require: any;
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 

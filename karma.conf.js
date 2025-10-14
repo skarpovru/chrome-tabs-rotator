ef90a3d8e1f7fd34359c5f29682634a51a6a4447
Stanislav Karpov <stanislav.karpov@palas.de>
2025-10-14 11:37:55 +0200

    ci(test): add ChromeHeadlessCI launcher and force headless in coverage script

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,36 @@
 // Karma configuration with coverage thresholds
 module.exports = function (config) {
+  const isCI = process.env.CI === 'true' || process.env.GITHUB_ACTIONS === 'true';
+
+  // Custom launcher tuned for containerized CI (no sandbox, disable GPU, etc.)
+  const customLaunchers = {
+    ChromeHeadlessCI: {
+      base: 'ChromeHeadless',
+      flags: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--disable-software-rasterizer',
+        '--no-first-run',
+        '--no-default-browser-check',
+        '--disable-background-networking',
+        '--disable-background-timer-throttling',
+        '--disable-renderer-backgrounding',
+        '--mute-audio'
+      ]
+    }
+  };
+
+  const browsersEnv = process.env.KARMA_BROWSERS;
+  const browsers = browsersEnv ? browsersEnv.split(',') : [isCI ? 'ChromeHeadlessCI' : 'ChromeHeadless'];
+
   config.set({
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     reporters: ['progress', 'coverage'],
+    customLaunchers,
+    browsers,
+    singleRun: true,
     coverageReporter: {
       dir: require('path').join(__dirname, 'coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
@@ -11,9 +39,9 @@ module.exports = function (config) {
         emitWarning: false,
         global: {
           statements: 70,
-            branches: 60,
-            functions: 70,
-            lines: 70
+          branches: 60,
+          functions: 70,
+          lines: 70
         }
       }
     }
